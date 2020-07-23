8b01a26a70dc20d42f972fdd62e2353420c05260
teemulin <63881696+teemulin@users.noreply.github.com>
2020-07-23 15:35:02 +0300

    added delete function

--- todolist/src/App.js
+++ todolist/src/App.js
@@ -15,6 +15,10 @@ function App() {
     setTodos([...todos, todo]);
   }
 
+  const doDelete = (index) => {
+    setTodos(todos.filter((todo, i) => i !== index));
+  }
+
   return (
     <div className="App">
       <form onSubmit={addToDo}>
@@ -22,7 +26,22 @@ function App() {
         Date: <input type="date" name="date" value={todo.date} onChange={inputChanged} />
         <input type="submit" value="Add" />
       </form>
-      <Todotable todos={todos} />
+      <table>
+        <tbody>
+          <tr>
+            <th>Description</th>
+            <th>Date</th>
+          </tr>
+
+          {todos.map((todo, index) =>
+            <tr key={index}>
+              <td>{todo.desc}</td>
+              <td>{todo.date}</td>
+              <td><input type="button" name="Delete" value="Delete" onClick={() => {doDelete(index);}} /> </td>
+            </tr>
+          )}
+        </tbody>
+        </table>
     </div>
   );
 }
--- todolist/src/components/Todolist.js
+++ todolist/src/components/Todolist.js
@@ -1,19 +1,21 @@
 import React from 'react';
 
 export default function Todolist(props) {
+    
     return (
         <div>
             <table>
                 <tbody>
                     <tr>
                         <th>Description</th>
-                        <th>Date</th>                            
+                        <th>Date</th>
                     </tr>
                     
                     {props.todos.map((todo, index) =>
                         <tr key={index}>
                         <td>{todo.desc}</td>
                         <td>{todo.date}</td>
+                        <td> <input type="button" name="Delete" value="Delete" /> </td>
                         </tr>
                         )
                     }
