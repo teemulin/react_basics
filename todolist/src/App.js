02f0a4bc7f3c898c8b36b7648fff06e4177aa7ad
teemulin <63881696+teemulin@users.noreply.github.com>
2020-07-23 13:22:11 +0300

    simple todo list

---
+++ todolist/src/App.js
@@ -0,0 +1,30 @@
+import React, { useState} from 'react';
+import './App.css';
+import Todotable from './components/Todolist';
+
+function App() {
+  const [todo, setTodo] = useState({desc:'', date:''});
+  const [todos, setTodos] = useState([]);
+
+  const inputChanged = (event) => {
+    setTodo({...todo, [event.target.name]: event.target.value});
+  }
+
+  const addToDo = (event) => {
+    event.preventDefault();
+    setTodos([...todos, todo]);
+  }
+
+  return (
+    <div className="App">
+      <form onSubmit={addToDo}>
+        Description: <input type="text" name="desc" value={todo.desc} onChange={inputChanged} />
+        Date: <input type="date" name="date" value={todo.date} onChange={inputChanged} />
+        <input type="submit" value="Add" />
+      </form>
+      <Todotable todos={todos} />
+    </div>
+  );
+}
+
+export default App;
---
+++ todolist/src/components/Todolist.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+export default function Todolist(props) {
+    return (
+        <div>
+            <table>
+                <tbody>
+                    <tr>
+                        <th>Description</th>
+                        <th>Date</th>                            
+                    </tr>
+                    
+                    {props.todos.map((todo, index) =>
+                        <tr key={index}>
+                        <td>{todo.desc}</td>
+                        <td>{todo.date}</td>
+                        </tr>
+                        )
+                    }
+                </tbody>
+            </table>
+        </div>
+    );
+}
