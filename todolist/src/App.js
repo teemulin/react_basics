51bf68e9569ef500a16f8ae99df67068ce97d364
teemulin <63881696+teemulin@users.noreply.github.com>
2020-07-23 18:41:50 +0300

    separating components

--- todolist/src/App.js
+++ todolist/src/App.js
@@ -1,6 +1,6 @@
 import React, { useState} from 'react';
 import './App.css';
-import Todotable from './components/Todolist';
+import Todolist from './components/Todolist';
 
 function App() {
   const [todo, setTodo] = useState({desc:'', date:''});
@@ -24,24 +24,10 @@ function App() {
       <form onSubmit={addToDo}>
         Description: <input type="text" name="desc" value={todo.desc} onChange={inputChanged} />
         Date: <input type="date" name="date" value={todo.date} onChange={inputChanged} />
-        <input type="submit" value="Add" />
+        <input type="submit" id="add" value="Add" />
       </form>
-      <table>
-        <tbody>
-          <tr>
-            <th>Description</th>
-            <th>Date</th>
-          </tr>
-
-          {todos.map((todo, index) =>
-            <tr key={index}>
-              <td>{todo.desc}</td>
-              <td>{todo.date}</td>
-              <td><input type="button" name="Delete" value="Delete" onClick={() => {doDelete(index);}} /> </td>
-            </tr>
-          )}
-        </tbody>
-        </table>
+      <Todolist todos={todos}
+                doDelete={doDelete} />
     </div>
   );
 }
--- todolist/src/components/Todolist.js
+++ todolist/src/components/Todolist.js
@@ -10,15 +10,14 @@ export default function Todolist(props) {
                         <th>Description</th>
                         <th>Date</th>
                     </tr>
-                    
+
                     {props.todos.map((todo, index) =>
                         <tr key={index}>
-                        <td>{todo.desc}</td>
-                        <td>{todo.date}</td>
-                        <td> <input type="button" name="Delete" value="Delete" /> </td>
+                            <td>{todo.desc}</td>
+                            <td>{todo.date}</td>
+                            <td><input type="button" id="delete" name="Delete" value="Delete" onClick={() => {props.doDelete(index);}} /> </td>
                         </tr>
-                        )
-                    }
+                    )}
                 </tbody>
             </table>
         </div>
