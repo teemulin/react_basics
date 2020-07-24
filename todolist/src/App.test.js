e4f64bd9aaca861f072a7a6ca9ed4e38673fdacb
teemulin <63881696+teemulin@users.noreply.github.com>
2020-07-24 16:55:26 +0300

    simple test learning

--- todolist/src/App.test.js
+++ todolist/src/App.test.js
@@ -1,9 +1,29 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import App from './App';
+import Todolist from './components/Todolist';
+import '@testing-library/jest-dom/extend-expect';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+test ('renders todolist', () => {
+  const row = [
+    {desc: 'Go to coffee', date: '24.11.2020'}
+  ]
+
+  const todolist = render(<Todolist todos={row} />);
+  expect(todolist.container).toHaveTextContent('Go to coffee');
+})
+
+test('add todo', () => {
+  const { container, getByText, getByPlaceholderText } = render(<App />);
+
+  const desc = getByPlaceholderText('Description');
+  fireEvent.change(desc, { target: { value: 'Go to coffee' } })
+
+  const date = getByPlaceholderText('Date');
+  fireEvent.change(date, { target: { value: '29.11.2019' } } )
+
+  const button = getByText('Add');
+  fireEvent.click(button);
+
+  expect(container).toHaveTextContent('Go to coffee');
+})
